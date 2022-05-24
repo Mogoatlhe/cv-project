7c2cb8163d1c8d0ea06ed04146e03e2d94f36dd5
mogoatlhe <iam@ofent.se>
2022-05-24 12:00:36 +0200

    added the cv container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Cv from  "./components/Cv";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Cv />
     </div>
   );
 }
---
+++ src/components/Cv.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Cv extends Component{
+    render(){
+        return(
+            <div id = "cv-container">
+            </div>
+        );
+    }
+}
+
+export default Cv;
