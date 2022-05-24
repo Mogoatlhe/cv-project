cf1de4366cedaf7e80245f33bcd98c7dddf70893
mogoatlhe <iam@ofent.se>
2022-05-24 14:27:06 +0200

    retrieving values from a state object.

--- src/components/CvHeader.js
+++ src/components/CvHeader.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 
 class CvHeader extends Component{
 
+    constructor(){
+        super();
+
+        this.state = {
+            name: "Name Surname",
+            role: "Proffesion/Role",
+            applicantDescr: "Brief description about yourself lorem lorem lorem lorem lorem lorem",
+            picture: false,
+        }
+    }
+
     render(){
         return(
             <div id = "cv-header-container">
                 <div id = "cv-header">
                     <div id = "cv-header-text">
-                        <p id = "name-surname">Name Surname</p>
-                        <p id = "role">Proffesion/Role</p>
-                        <p id = "applicant-description">Brief description about yourself lorem lorem lorem lorem lorem lorem</p>
+                        <p id = "name-surname"> { this.state.name } </p>
+                        <p id = "role"> { this.state.role } </p>
+                        <p id = "applicant-description"> { this.state.applicantDescr } </p>
                     </div>
                     <div id = "profile-picture">
                         <p id = "drag-and-drop-text">drag &amp; drop</p>
